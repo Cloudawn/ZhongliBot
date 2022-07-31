fa1fda4fb27aff36368132af551174d483708227
bingyue <99388013+bingqiu456@users.noreply.github.com>
2022-07-31 10:05:34 +0800

    👁更新查询海外
    已完成二分之四！

--- src/plugins/covid_19/__init__.py
+++ src/plugins/covid_19/__init__.py
@@ -1,9 +1,14 @@
 from nonebot import  on_command
 from nonebot.params import CommandArg
-from . import domestic
+from . import domestic,foreign
 from nonebot.adapters.onebot.v11 import Message
 
 search_covid_19 = on_command("查询疫情",aliases={"疫情地区"})
 @search_covid_19.handle()
 async def _(foo:Message=CommandArg()):
     await search_covid_19.finish(await domestic.httpx_covid_city(msg=str(foo)))
+
+search_covid = on_command("海外疫情")
+@search_covid.handle()
+async def _(foo:Message=CommandArg()):
+    await search_covid.finish(await foreign.httpx_covid_city(msg=str(foo)))
