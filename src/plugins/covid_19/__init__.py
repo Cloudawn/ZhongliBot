021afe47642e8aaea3babb7bcc25f64b4668c4d9
丙实 <1300432792@qq.com>
2022-07-31 14:13:15 +0800

    🏥简化疫情查询的指令，变更为``疫情xxx``

--- src/plugins/covid_19/__init__.py
+++ src/plugins/covid_19/__init__.py
@@ -3,7 +3,7 @@ from nonebot.params import CommandArg
 from . import domestic
 from nonebot.adapters.onebot.v11 import Message
 
-search_covid_19 = on_command("查询疫情",aliases={"疫情地区"})
+search_covid_19 = on_command("疫情",aliases={"查询疫情"})
 @search_covid_19.handle()
 async def _(foo:Message=CommandArg()):
     await search_covid_19.finish(await domestic.httpx_covid_city(msg=str(foo)))
