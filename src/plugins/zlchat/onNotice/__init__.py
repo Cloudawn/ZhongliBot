8f28646cc66b223e263ede8a9f550bc7b9a2db2d
丙实 <1300432792@qq.com>
2022-07-31 22:32:26 +0800

    ✍将``进群欢迎``移动至``group_member_manage``中

--- src/plugins/zlchat/onNotice/__init__.py
+++ src/plugins/zlchat/onNotice/__init__.py
@@ -1,27 +1,19 @@
 import os
 import random
 
-from nonebot import on, on_notice
-from nonebot.adapters.onebot.v11 import (Bot, GroupIncreaseNoticeEvent,
-                                         Message, MessageSegment,
-                                         PokeNotifyEvent)
+from nonebot import  on_notice
+from nonebot.adapters.onebot.v11 import (Bot, GroupAdminNoticeEvent,
+                                         GroupIncreaseNoticeEvent, Message,
+                                         MessageSegment, PokeNotifyEvent)
 from nonebot.exception import FinishedException
 from nonebot.internal.matcher import Matcher
+from src.modules.user_info import UserInfo
 from src.utils.config import config
 from src.utils.log import logger
 
-from .data import zl_send_welcome
 
-welcome = on_notice(priority=1, block=False)
 
 poke = on_notice(priority=5, block=False)
-
-
-@welcome.handle()
-async def _(bot: Bot, event: GroupIncreaseNoticeEvent):
-    logger.debug(f"<y>用户{event.user_id}</y>加入了<y>群{event.group_id}</y>")
-    msg = await zl_send_welcome(welcome, event)
-    await welcome.finish(msg)
 
 
 @poke.handle()
@@ -39,3 +31,6 @@ async def _poke(bot: Bot, event: PokeNotifyEvent, matcher: Matcher):
     else:
         qq = event.user_id
         await poke.finish(MessageSegment("poke", {"qq": qq}))
+
+
+
