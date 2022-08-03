1257b360226f86ab597897c7e0475c17da93c896
bingyue <99388013+bingqiu456@users.noreply.github.com>
2022-08-03 23:40:58 +0800

    👁与search_city.py同上

--- src/plugins/covid_19/__init__.py
+++ src/plugins/covid_19/__init__.py
@@ -6,8 +6,8 @@ from . import domestic, foreign
 
 Export = export()
 Export.plugin_name = "疫情查询"
-Export.plugin_command = "疫情xx | 海外疫情xx"
-Export.plugin_usage = "查询疫情信息，新增、死亡等；待更新风险地区"
+Export.plugin_command = "疫情xx | 海外疫情xx|查风险xxx"
+Export.plugin_usage = "查询疫情信息，新增、死亡等，风险地区"
 Export.default_status = True
 
 search_covid_19 = on_command("疫情", aliases={"查询疫情"})
@@ -23,3 +23,10 @@ search_covid = on_command("海外疫情")
 @search_covid.handle()
 async def _(foo: Message = CommandArg()):
     await search_covid.finish(await foreign.httpx_covid_city(msg=str(foo)))
+
+search_danger = on_command("查风险")
+
+
+@search_danger.handle()
+async def _(foo:Message=CommandArg()):
+    await search_danger.finish(await search_city.search_city(foo=str(foo)))
